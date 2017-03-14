2a38899ca6d60d2e8138229e9db2c30b87dca095
mondherdz <mondher.1jam@gmail.com>
2017-03-14 09:16:31 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -26,6 +26,31 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+    
+var admobid = {};
+  if( /(android)/i.test(navigator.userAgent) ) { // for android & amazon-fireos
+    admobid = {
+      banner: 'ca-app-pub-7031979927877857/4725667524', // or DFP format "/6253334/dfp_example_ad"
+      interstitial: 'ca-app-pub-xxx/yyy'
+    };
+  } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) { // for ios
+    admobid = {
+      banner: 'ca-app-pub-7031979927877857/4725667524', // or DFP format "/6253334/dfp_example_ad"
+      interstitial: 'ca-app-pub-xxx/kkk'
+    };
+  } else { // for windows phone
+    admobid = {
+      banner: 'ca-app-pub-7031979927877857/4725667524', // or DFP format "/6253334/dfp_example_ad"
+      interstitial: 'ca-app-pub-xxx/kkk'
+    };
+  }
+
+  // it will display smart banner at top center, using the default options
+if(AdMob) AdMob.createBanner({
+  adId: admobid.banner,
+  position: AdMob.AD_POSITION.BOTTOM_CENTER,
+  isTesting : true,
+  autoShow: true });
   });
 })
 
